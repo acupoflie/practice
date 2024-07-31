bb720ca4810ed2466e4a2b17778b325ac1a69d0e
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-01 02:44:38 +0400

    crud operations

--- expressjs/controllers/moviesController.js
+++ expressjs/controllers/moviesController.js
@@ -30,12 +30,41 @@ exports.validateBody = (req, res, next) => {
     next()
 }
 
-exports.getAllMovies = (req, res) => {
+exports.getAllMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find()
 
+        res.status(200).json({
+            status: 'success',
+            length: movies.length,
+            data: {
+                movies
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
 }
 
-exports.getMovie = (req, res) => {
+exports.getMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id)
 
+        res.status(200).json({
+            status: 'success',
+            data: {
+                movie
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
 }
 
 exports.createMovie = async (req, res) => {
@@ -57,10 +86,36 @@ exports.createMovie = async (req, res) => {
     }
 }
 
-exports.updateMovie = (req, res) => {
-
+exports.updateMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        
+        res.status(200).json({
+            status: 'success',
+            data: {
+                movie
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
 }
 
-exports.deleteMovie = (req, res) => {
+exports.deleteMovie = async (req, res) => {
+    try {
+        await Movie.findByIdAndDelete(req.params.id)
 
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
 }
