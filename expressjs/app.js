d4026c915a7793a39b2de61914be773a21d0ea2a
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-05 23:39:48 +0400

    default route

--- expressjs/app.js
+++ expressjs/app.js
@@ -26,6 +26,13 @@ app.use(express.static('./public'))
 
 app.use('/api/v1/movies', moviesRouter)
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on the server`
+    })
+})
+
 module.exports = app
 
 
