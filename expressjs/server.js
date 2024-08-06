2568b1d2455975541a1825915a94e58d6b6d1c6e
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-06 16:43:16 +0400

    handling unhandled rejection

--- expressjs/server.js
+++ expressjs/server.js
@@ -6,11 +6,17 @@ const app = require('./app')
 
 mongoose.connect(process.env.CONN_STR,).then((conn) => {
     console.log('db connection succesfull')
-}).catch((err) => {
-    console.log('Some error has occured')
 })
 
 const PORT = 3000 || process.env.PORT
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log('server has started')
+})
+
+process.on('unhandledRejection', (err) => {
+    console.log(err.name, err.message)
+    //! proper way shut down the server
+    server.close(() => {
+        process.exit(1)
+    })
 })
