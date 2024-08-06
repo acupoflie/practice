19c726eea8e0ae04d5f2cb837318c171e96668bc
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-07 00:03:28 +0400

    signup with jwt

--- expressjs/models/userModel.js
+++ expressjs/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require("validator")
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,8 +22,22 @@ const userSchema = new mongoose.Schema({
     },
     confirmPassword: {
         type: String,
-        required: [true, 'Confirm your password']
+        required: [true, 'Confirm your password'],
+        validate: {
+            validator: function(value) {
+                return this.password == value
+            },
+            message: 'Passwords are not same!'
+        }
     }
+})
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password, 12)
+    this.confirmPassword = undefined
+    next()
 })
 
 const User = mongoose.model('User', userSchema);
