5f35e89798dc48c143a3b046a35251c848ecfa7a
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-09-04 21:52:48 +0400

    using jwt from scratch and with jsonwebtoken

--- passportjs/JWT/issueJWT.js
+++ passportjs/JWT/issueJWT.js
@@ -1,8 +1,11 @@
 const base64url = require('base64url')
 const crypto = require('crypto')
 const signatureFunction = crypto.createSign('RSA-SHA256')
+const verifyFunction = crypto.createVerify('RSA-SHA256')
 const fs = require('fs')
-const {sign} = require("crypto");
+
+
+// ISSUE JWT
 
 const headerObj = {
     alg: 'RS256',
@@ -31,21 +34,27 @@ const signatureBase64 = signatureFunction.sign(PRIV_KEY, 'base64')
 const signatureBase64Url = base64url.fromBase64(signatureBase64)
 
 
+// VERIFICATION
+
+const jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ'
+
+const jwtParts = jwt.split('.')
+
+const headerInBase64UrlFormat = jwtParts[0];
+const payloadInBase64UrlFormat = jwtParts[1];
+const signatureInBase64UrlFormat = jwtParts[2];
+
+verifyFunction.write(base64UrlHeader + '.' + base64UrlPayload);
+verifyFunction.end()
+
+const jwtSignaturesBase64 = base64url.toBase64(signatureInBase64UrlFormat)
+
+const PUB_KEY = fs.readFileSync(__dirname + '/pub_key.pem', 'utf-8')
+const signatureIsValid = verifyFunction.verify(PUB_KEY, jwtSignaturesBase64, 'base64')
+
+console.log(signatureIsValid)
 
 
 
-// const jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ'
-//
-// const jwtParts = jwt.split('.')
-//
-// const headerInBase64UrlFormat = jwtParts[0];
-// const payloadInBase64UrlFormat = jwtParts[1];
-// const signatureInBase64UrlFormat = jwtParts[2];
-//
-// const decodedHeader = base64url.decode(headerInBase64UrlFormat)
-// const decodedPayload = base64url.decode(payloadInBase64UrlFormat)
-// const decodedSignature = base64url.decode(signatureInBase64UrlFormat)
-//
-// console.log(decodedHeader)
-// console.log(decodedPayload)
-// console.log(decodedSignature)
+
+
