725df3cbce74fb3bea2895d9e1340ae5db0ef4f8
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-09-14 21:31:34 +0400

    enviroment variables

--- docker-with-nodejs/index.js
+++ docker-with-nodejs/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose.connect(mongoURL)
+    .then(() => console.log('db connection succesfull')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('<h2>hi there...!!!</h2>')
